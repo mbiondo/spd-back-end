5b50626a9bdc3c2d4ab39798a80ce2eb4af70a93
FiubaDaniel <fiubadaniel@gmail.com>
2015-04-14 12:56:15 -0300

    Para el servicio de expediente uso el modelo GIRO para obtener los giros de un expediente, para no tener que utilizar la vista.

--- ServiciosParlamentarios/apirest/serializers/expedientes/expediente.py
+++ ServiciosParlamentarios/apirest/serializers/expedientes/expediente.py
@@ -1,12 +1,14 @@
 from rest_framework import serializers
 from apirest.models.expedientes.expediente import Expediente
 from apirest.serializers.db_views.firmantes import FirmanteSerializer
-from apirest.serializers.db_views.giros import GirosSerializer
+# from apirest.serializers.db_views.giros import GirosSerializer
+from apirest.serializers.giro import GiroSerializer
 
 class ExpedienteSerializer(serializers.ModelSerializer):
     
     firmantes = FirmanteSerializer(many=True)
-    giros = GirosSerializer(many=True)
+    #giros = GirosSerializer(many=True)    
+    giros = GiroSerializer(many=True)
     
     class Meta:
         model = Expediente
