f11e08062d72e00c2245e0cffa07773e2677194f
Giovanni Opromolla <giopromolla@gmail.com>
2015-04-30 17:50:20 -0300

    ordeno urls

--- ServiciosParlamentarios/apirest/urls.py
+++ ServiciosParlamentarios/apirest/urls.py
@@ -16,25 +16,23 @@ from apirest.views.organismos.despacho import DespachoViewSet
 
 router = DefaultRouter()
 
+router.register(r'bloques', bloque.BloqueViewSet)
 router.register(r'cargos', cargo.CargoViewSet)
+router.register(r'citaciones', CitacionViewSet)
+router.register(r'comisiones', comision.ComisionViewSet)
+router.register(r'despachos',DespachoViewSet)
+router.register(r'dictamenes', DictamenViewSet)
+router.register(r'expedientes', expediente.ExpedienteViewSet)
+router.register(r'legisladores', legislador.LegisladorViewSet)
 router.register(r'personas_fisicas', persona_fisica.PersonaFisicaViewSet)
 router.register(r'personas_fisicas_detalle', PersonaFisicaDetalleViewSet)
+router.register(r'proyectos', ProyectosViewSet)
 # router.register(r'personas_fisicas', persona_fisica.PersonaFisicaFullViewSet)
 # router.register(r'personas_fisicas_datos_actuales', persona_fisica.PersonaFisicaActualViewSet)
-router.register(r'legisladores', legislador.LegisladorViewSet)
 # router.register(r'legisladores', legislador.LegisladorComisionViewSet)
-router.register(r'expedientes', expediente.ExpedienteViewSet)
-router.register(r'comisiones', comision.ComisionViewSet)
 # router.register(r'comisiones_historico', comision.ComisionHistoricoViewSet)
 # router.register(r'comisiones_detalle', comision.ComisionDetalleViewSet)
-router.register(r'bloques', bloque.BloqueViewSet)
-router.register(r'proyectos', ProyectosViewSet)
 # router.register(r'bloques_detalle', bloque.BloqueDetalleViewSet)
-
-router.register(r'citaciones', CitacionViewSet)
-
-router.register(r'dictamenes', DictamenViewSet)
-router.register(r'despachos',DespachoViewSet)
 
 
 expediente_router = routers.NestedSimpleRouter(router, r'expedientes', lookup='expediente')
