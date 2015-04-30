adff8b43a70dc55b09dde9d36560fd578a163831
Giovanni Opromolla <giopromolla@gmail.com>
2015-04-30 17:42:28 -0300

    cambio servicio dictamenes para que sea RESTful

--- ServiciosParlamentarios/apirest/serializers/expedientes/dictamen.py
+++ ServiciosParlamentarios/apirest/serializers/expedientes/dictamen.py
@@ -1,12 +1,8 @@
 from rest_framework import serializers
 from apirest.models.expedientes.dictamen import Dictamen
-from apirest.serializers.organismos.despacho import DespachoSerializer
 
 class DictamenSerializer(serializers.ModelSerializer):
     
-    despacho = DespachoSerializer()
-    
     class Meta:
         model = Dictamen
-        fields = ('id','despacho','tipo','copete','accion','bconmodificacion')           
-        
+        fields = ('id','despacho','tipo','copete','accion','bconmodificacion')       
