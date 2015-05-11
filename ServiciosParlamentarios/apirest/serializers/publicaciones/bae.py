82fccbc3651be546efdfbffb350dcaa6ac133694
FiubaDaniel <fiubadaniel@gmail.com>
2015-05-11 11:34:42 -0300

    Borro modelos de la vieja estructura de publicacion que ya no son utilizados.

--- ServiciosParlamentarios/apirest/serializers/publicaciones/bae.py
+++
@@ -1,8 +0,0 @@
-from rest_framework import serializers
-from apirest.models.publicaciones.bae import Bae
-
-class BaeSerializer(serializers.ModelSerializer):
-        
-    class Meta:
-        model = Bae
-#         fields = ('id','fk_publicacion_estructura','numero','fhapertura','fhcierre','fk_camara_reunion','tipo')
--- ServiciosParlamentarios/apirest/serializers/publicaciones/tp_dae.py
+++
@@ -1,8 +0,0 @@
-from rest_framework import serializers
-from apirest.models.publicaciones.tp_dae import TpDae
-
-class TpDaeSerializer(serializers.ModelSerializer):
-        
-    class Meta:
-        model = TpDae
-#         fields = ('id','fk_publicacion_estructura','fk_bae_dae','fhapertura','fhcierre')
