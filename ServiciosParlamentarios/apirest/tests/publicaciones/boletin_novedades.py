004126fd63e45271f1e71a0223c33da50cb8b9bb
FiubaDaniel <fiubadaniel@gmail.com>
2015-05-12 16:50:18 -0300

    Agrego los Tests de BN y DAE. BN no tiene datos en su tabla, se crean los tests comentados. Al momento de completar la tabla hay que actualizar las constantes.

---
+++ ServiciosParlamentarios/apirest/tests/publicaciones/boletin_novedades.py
@@ -0,0 +1,40 @@
+# -*- coding: utf-8 -*-
+from rest_framework.test import APITestCase
+
+class TestBoletinNovedades(APITestCase):
+    
+    # LA TABLA NO TIENE DATOS. QUEDAN LOS TESTS COMENTADOS. ACTULIZAR LAS CONSTANTES CUANDO SE COMPLETE LA TABLA.
+    
+    #Constantes usadas para realizar la comparacion en los test.
+    CODIGO_EXITO = 200 
+    CANTIDAD_BN = 735
+    CANTIDAD_BN_POR_NUMERO = 16
+    NUMERO = 1
+#     TIPO_CAMARA = 
+#     
+#     def test_bn_list(self):
+#         """
+#         Prueba el servicio para listar los BN. 
+#         """
+#         response = self.client.get('/apirest/boletin_novedades/')
+#         self.assertEqual(response.status_code, self.CODIGO_EXITO)
+#         self.assertEqual(response.data["count"], self.CANTIDAD_BAE)
+#         
+#     def test_bn_por_numero(self):
+#         """
+#         Prueba el servicio para obtener un BN filtrando por numero.
+#         """
+#         response = self.client.get("/apirest/boletin_novedades/?numero=1")
+#         self.assertEqual(response.status_code, self.CODIGO_EXITO)
+#         self.assertEqual(response.data["count"], self.CANTIDAD_BAE_POR_NUMERO)
+#         self.assertEqual(response.data["results"][0]["numero"], self.NUMERO)
+#         self.assertEqual(response.data["results"][0]["tipo_camara"], self.TIPO_CAMARA)
+#         
+#     def test_bn_por_id(self):
+#         """
+#         Prueba el servicio para obtener un BAT por id. 
+#         """
+#         response = self.client.get("/apirest/boletin_novedades/XXX/")
+#         self.assertEqual(response.status_code, self.CODIGO_EXITO)
+#         self.assertEqual(response.data["numero"], self.NUMERO)
+#         self.assertEqual(response.data["tipo_camara"], self.TIPO_CAMARA)
---
+++ ServiciosParlamentarios/apirest/tests/publicaciones/diario_asuntos_entrados.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+from rest_framework.test import APITestCase
+
+class TestDiarioAsuntosEntrados(APITestCase):
+    
+    #Constantes usadas para realizar la comparacion en los test.
+    CODIGO_EXITO = 200 
+    CANTIDAD_DAE = 3287
+    CANTIDAD_DAE_POR_NUMERO = 28
+    NUMERO = 1
+
+    def test_bn_list(self):
+        """
+        Prueba el servicio para listar los DAE. 
+        """
+        response = self.client.get('/apirest/diario_asuntos_entrados/')
+        self.assertEqual(response.status_code, self.CODIGO_EXITO)
+        self.assertEqual(response.data["count"], self.CANTIDAD_DAE)
+
+    def test_bn_por_numero(self):
+        """
+        Prueba el servicio para obtener un BN filtrando por numero.
+        """
+        response = self.client.get("/apirest/diario_asuntos_entrados/?numero=1")
+        self.assertEqual(response.status_code, self.CODIGO_EXITO)
+        self.assertEqual(response.data["count"], self.CANTIDAD_DAE_POR_NUMERO)
+        self.assertEqual(response.data["results"][0]["numero"], self.NUMERO)
+         
+    def test_bn_por_id(self):
+        """
+        Prueba el servicio para obtener un BAT por id. 
+        """
+        response = self.client.get("/apirest/diario_asuntos_entrados/45908/")
+        self.assertEqual(response.status_code, self.CODIGO_EXITO)
+        self.assertEqual(response.data["numero"], self.NUMERO)
