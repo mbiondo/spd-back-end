45fd458d697e60d9a590a3c097e911a84d0db1f7
Giovanni Opromolla <giopromolla@gmail.com>
2015-05-27 12:39:49 -0300

    Elimino filtro de tipo en Tramite Parlamentario

--- ServiciosParlamentarios/apirest/filters/publicaciones/tramite_parlamentario.py
+++ ServiciosParlamentarios/apirest/filters/publicaciones/tramite_parlamentario.py
@@ -8,10 +8,9 @@ class TramiteParlamentarioFilter(django_filters.FilterSet):
     fecha_hora_apertura = django_filters.DateTimeFilter(name="fecha_hora_apertura")
     fecha_hora_cierre = django_filters.DateTimeFilter(name="fecha_hora_cierre")
     numero = django_filters.NumberFilter(name="numero")
-    visibilidad = django_filters.NumberFilter(lookup_type='icontains',name="visibilidad")
+    visibilidad = django_filters.NumberFilter(name="visibilidad")
     fecha_impresion = django_filters.DateTimeFilter(name="fecha_impresion")
-    tipo = django_filters.CharFilter(lookup_type='icontains',name="tipo")
             
     class Meta:
         model = TramiteParlamentario
-        fields = ['fecha_hora_apertura', 'fecha_hora_cierre','numero']
+        fields = ['fecha_hora_apertura', 'fecha_hora_cierre','numero','visibilidad','fecha_impresion']
