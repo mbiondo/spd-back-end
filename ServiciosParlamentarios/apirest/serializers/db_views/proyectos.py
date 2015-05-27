14640498a6d5008a7343235735eabe45dc5e407b
FiubaDaniel <fiubadaniel@gmail.com>
2015-05-27 16:00:13 -0300

    Remove proyectos (usaba una vista)

--- ServiciosParlamentarios/apirest/serializers/db_views/proyectos.py
+++
@@ -1,8 +0,0 @@
-from rest_framework import serializers
-from apirest.models.db_views.proyectos import Proyectos
-
-class ProyectoSerializer(serializers.ModelSerializer):
-    
-    class Meta: 
-        model = Proyectos
-        fields = ('expediente_id','codigo_exp','codigo_num','codigo_origen','codigo_anio','sumario','tipo_camara','tipo','fecha_caducidad','fecha','periodo','titulo','voces','resultado','tipo_proy','nro_ley')  
