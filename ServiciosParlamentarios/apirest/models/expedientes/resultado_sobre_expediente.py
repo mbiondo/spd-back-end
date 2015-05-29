fdfc8612fbee10706d4ab3c8efd0c0add4727cf7
Giovanni Opromolla <giopromolla@gmail.com>
2015-05-29 13:22:45 -0300

    Fix de citaciones, correccion de resultado-relacion

--- ServiciosParlamentarios/apirest/models/expedientes/resultado_sobre_expediente.py
+++ ServiciosParlamentarios/apirest/models/expedientes/resultado_sobre_expediente.py
@@ -4,8 +4,8 @@ from apirest.utils.constants import Constants
 
 class ResultadoSobreExpediente(models.Model):
     id = models.AutoField(primary_key=True,db_column='resultado_sobre_expediente_id')
-    fk_resultado = models.ForeignKey('Resultado', db_column='fk_resultado')
-    fk_expediente = models.ForeignKey('Proyecto', db_column='fk_expediente')
+    resultado = models.ForeignKey('Resultado', db_column='fk_resultado')
+    expediente = models.ForeignKey('Proyecto', db_column='fk_expediente')
     
     class Meta:
         managed = False
