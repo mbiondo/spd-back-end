d2bfaecfed81a2eea59f418460ac479c5726a2b4
Giovanni Opromolla <giopromolla@gmail.com>
2015-06-02 19:22:04 -0300

    filtros gpa

---
+++ ServiciosParlamentarios/apirest/filters/organismos/gpa_filter.py
@@ -0,0 +1,17 @@
+from rest_framework.compat import django_filters
+from apirest.filters.custom_filter_list import CustomFilterList
+from apirest.models.organismos.grupo_parlamentario_amistad import GrupoParlamentarioAmistad
+
+class GrupoParlamentarioAmistadFilter(django_filters.FilterSet):
+    
+    # Grupo Parlamentario de Amistad filters.
+    id = CustomFilterList(name="id", lookup_type="in")
+    caracter = django_filters.CharFilter(lookup_type='icontains',name="caracter")
+    tipo_camara = django_filters.CharFilter(lookup_type='icontains',name="tipo_camara")
+    nombre = django_filters.CharFilter(lookup_type='icontains',name="historico__nombre")
+    nombre_corto = django_filters.CharFilter(lookup_type='icontains',name="historico__nombre_corto")
+      
+    class Meta:
+        model = GrupoParlamentarioAmistad
+        fields = ['id','caracter','tipo_camara','nombre','nombre_corto']
+                
