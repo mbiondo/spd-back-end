e2795494b06cbbe0dd04ce21d63fc3bcbe5b73fe
Giovanni Opromolla <giopromolla@gmail.com>
2015-06-05 12:57:29 -0300

    Fix firmantes

--- ServiciosParlamentarios/apirest/models/db_views/firmantes.py
+++ ServiciosParlamentarios/apirest/models/db_views/firmantes.py
@@ -6,9 +6,9 @@ from apirest.models.expedientes.proyecto import Proyecto
 
 class Firmantes(models.Model):
     
+    id = models.IntegerField(blank=True, null=True,primary_key=True, db_column='cargo_persona_fisica_id')
     proyecto = models.ForeignKey(Proyecto, db_column='expediente_id',related_name='firmantes')
-    id = models.IntegerField(blank=True, null=True,primary_key=True,db_column='persona_fisica_id')
-    cargo_persona_fisica_id = models.IntegerField(blank=True, null=True)
+    persona_fisica_id = models.IntegerField(blank=True, null=True)
     cargo_tipo = models.TextField(blank=True)
     codigo_exp = models.CharField(max_length=14, blank=True,db_column='codigoexp')
     nombre_leg_func = models.TextField(blank=True)
