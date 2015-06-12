257efc6971e3d1a5e01c4e11f27f601f69905cf8
Giovanni Opromolla <giopromolla@gmail.com>
2015-06-12 17:55:26 -0300

    Girosapirest/views/giro.py

---
+++ ServiciosParlamentarios/apirest/filters/giro_filter.py
@@ -0,0 +1,12 @@
+from rest_framework.compat import django_filters
+from apirest.filters.custom_filter_list import CustomFilterList
+from apirest.models.giro import Giro
+
+class GiroFilter(django_filters.FilterSet):
+    
+    id = CustomFilterList(name="id", lookup_type="in")
+        
+    class Meta:
+        model = Giro
+        fields = ['id',]
+
