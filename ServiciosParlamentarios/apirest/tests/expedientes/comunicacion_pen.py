db2b3d903db4e90421118b17fa7b2f4def266c88
FiubaDaniel <fiubadaniel@gmail.com>
2015-06-15 12:38:08 -0300

    Agrego los test del servicio comunicacion pen.

--- ServiciosParlamentarios/apirest/tests/expedientes/comunicacion_pen.py
+++ ServiciosParlamentarios/apirest/tests/expedientes/comunicacion_pen.py
@@ -1,97 +1,92 @@
 # -*- coding: utf-8 -*-
 from rest_framework.test import APITestCase
 
-class TestComunicacion(APITestCase):
+class TestComunicacionPen(APITestCase):
     
     # Constantes para corroborar el resultado de los test
-    CANT_COMUNICACIONES = 8059
-    CANT_TIPO_CAMARA = 4972
-    CANT_CODIGO_ORIGEN= 4689
+    CANT_COMUNICACIONES_PEN = 5298
             
     CODIGO_EXITO = 200
-    TIPO = "COMUNICACION"
-    CODIGO_NUM = '0114'  
+    TIPO = "COMUNICACION_PEN"
+    CODIGO_NUM = '0001'  
     TIPO_CAMARA = "Diputados"
-    CODIGO_EXP = "0114-S-1997"
-    CODIGO_ANIO = "1997"
-    PERIODO = 115
-    CODIGO_ORIGEN = 'S'
+    CODIGO_EXP = "0001-JGM-1995"
+    CODIGO_ANIO = "2002"
+    CODIGO_ANIO_CODIGO_NUM = "1995"
+    PERIODO = 113
+    CODIGO_ORIGEN = 'JGM'
     CODIGO_ANIO_ORDEN = "2002"
         
-    # Test: Servicio de comunicaciones
-    def test_comunicacion(self):
+    # Test: Servicio de cominicaciones pen
+    def test_comunicacion_pen(self):
         """
-        Prueba servicio de comunicaciones.  
+        Prueba servicio de comunicaciones_pen.  
         """
-        response = self.client.get('/apirest/comunicaciones/')
+        response = self.client.get('/apirest/comunicaciones_pen/')
         self.assertEqual(response.status_code, self.CODIGO_EXITO)
-        self.assertEqual(response.data["count"], self.CANT_COMUNICACIONES)
+        self.assertEqual(response.data["count"], self.CANT_COMUNICACIONES_PEN)
         self.assertEqual(response.data["results"][0]["tipo"], self.TIPO)
         self.assertEqual(response.data["results"][0]["tipo_camara"], self.TIPO_CAMARA)
         
-    def test_comunicacion_id(self):
+    def test_comunicacion_pen_id(self):
         """
-        Prueba el servicio para traer una comunicacion por id
+        Prueba el servicio para traer una comunicacion_pen por id
         """
-        response = self.client.get('/apirest/comunicaciones/95489/')
+        response = self.client.get('/apirest/comunicaciones_pen/1/')
         self.assertEqual(response.status_code, self.CODIGO_EXITO)
         self.assertEqual(response.data["codigo_num"], self.CODIGO_NUM)
         self.assertEqual(response.data["tipo"], self.TIPO)
         self.assertEqual(response.data["tipo_camara"], self.TIPO_CAMARA)
         
-    # Test sobre filtros. 
-        
-    def test_comunicacion_tipo_camara(self):
+    def test_comunicacion_pen_tipo_camara(self):
         """
-        Prueba que traiga las comunicaciones filtrando por tipo de camara.
+        Prueba que traiga las comunicaciones_pen filtrando por tipo de camara.
         """
-        response = self.client.get('/apirest/comunicaciones/?tipo_camara=Diputados')
+        response = self.client.get('/apirest/comunicaciones_pen/?tipo_camara=Diputados')
         self.assertEqual(response.status_code, self.CODIGO_EXITO)
-        self.assertEqual(response.data["count"], self.CANT_TIPO_CAMARA)
         self.assertEqual(response.data["results"][0]["tipo_camara"], self.TIPO_CAMARA)
         self.assertEqual(response.data["results"][0]["tipo"], self.TIPO)
         self.assertEqual(response.data["results"][0]["codigo_num"], self.CODIGO_NUM)  
         
-    def test_comunicacion_codigo_origen(self):
+    def test_comunicacion_pen_codigo_origen(self):
         """
-        Prueba que traiga las comunicaciones filtrando por codigo de origen.
+        Prueba que traiga las comunicaciones_pen filtrando por codigo de origen.
         """
-        response = self.client.get('/apirest/comunicaciones/?codigo_origen=S')
+        response = self.client.get('/apirest/comunicaciones_pen/?codigo_origen=JGM')
         self.assertEqual(response.status_code, self.CODIGO_EXITO)
-        self.assertEqual(response.data["count"], self.CANT_CODIGO_ORIGEN)
         self.assertEqual(response.data["results"][0]["tipo"], self.TIPO)
         self.assertEqual(response.data["results"][0]["codigo_num"], self.CODIGO_NUM) 
         self.assertEqual(response.data["results"][0]["tipo_camara"], self.TIPO_CAMARA)
         self.assertEqual(response.data["results"][0]["codigo_origen"], self.CODIGO_ORIGEN)
 
-    def test_comunicacion_codigo_exp(self):
+    def test_comunicacion_pen_codigo_exp(self):
         """
-        Prueba que traiga las comunicaciones filtrando por codigo de expediente.
+        Prueba que traiga las comunicaciones_pen filtrando por codigo de expediente.
         """
-        response = self.client.get('/apirest/comunicaciones/?codigo_exp=0114-S-1997')
+        response = self.client.get('/apirest/comunicaciones_pen/?codigo_exp=0001-JGM-1995')
         self.assertEqual(response.status_code, self.CODIGO_EXITO)
         self.assertEqual(response.data["results"][0]["codigo_exp"], self.CODIGO_EXP)
         self.assertEqual(response.data["results"][0]["tipo"], self.TIPO)
         self.assertEqual(response.data["results"][0]["codigo_num"], self.CODIGO_NUM) 
         self.assertEqual(response.data["results"][0]["tipo_camara"], self.TIPO_CAMARA)
         
-    def test_comunicacion_codigo_num(self):
+    def test_comunicacion_pen_codigo_num(self):
         """
-        Prueba que traiga las comunicaciones filtrando por codigo de numero.
+        Prueba que traiga las comunicaciones_pen filtrando por codigo de numero.
         """        
-        response = self.client.get('/apirest/comunicaciones/?codigo_num=0114')
+        response = self.client.get('/apirest/comunicaciones_pen/?codigo_num=0001')
         self.assertEqual(response.status_code, self.CODIGO_EXITO)
         self.assertEqual(response.data["results"][0]["codigo_num"], self.CODIGO_NUM)
         self.assertEqual(response.data["results"][0]["tipo"], self.TIPO)
         self.assertEqual(response.data["results"][0]["codigo_exp"], self.CODIGO_EXP)
         self.assertEqual(response.data["results"][0]["tipo_camara"], self.TIPO_CAMARA)
-        self.assertEqual(response.data["results"][0]["codigo_anio"], self.CODIGO_ANIO)      
+        self.assertEqual(response.data["results"][0]["codigo_anio"], self.CODIGO_ANIO_CODIGO_NUM)      
         
-    def test_comunicacion_periodo(self):
+    def test_comunicacion_pen_periodo(self):
         """
-        Prueba que traiga las comunicaciones filtrando por periodo
+        Prueba que traiga las comunicaciones_pen filtrando por periodo
         """                  
-        response = self.client.get('/apirest/comunicaciones/?periodo=115')
+        response = self.client.get('/apirest/comunicaciones_pen/?periodo=113')
         self.assertEqual(response.status_code, self.CODIGO_EXITO)
         self.assertEqual(response.data["results"][0]["periodo"], self.PERIODO)
         self.assertEqual(response.data["results"][0]["tipo"], self.TIPO) 
@@ -101,51 +96,40 @@ class TestComunicacion(APITestCase):
             
     def test_fecha_desde_y_hasta(self):
         """
-        Prueba que traiga las comunicaciones que pertenecen al rango de fecha indicada.
+        Prueba que traiga las comunicaciones_pen que pertenecen al rango de fecha indicada.
         """
-        response = self.client.get('/apirest/comunicaciones/?fechaDesde=2015-01-01&fechaHasta=2015-04-01')
+        response = self.client.get('/apirest/comunicaciones_pen/?fechaDesde=2015-01-01&fechaHasta=2015-04-01')
         self.assertEqual(response.status_code, self.CODIGO_EXITO)
         self.assertEqual(response.data["results"][0]["tipo"], self.TIPO)
         
-    def test_comunicacion_subtipo(self):
+    def test_comunicacion_pen_subtipo(self):
         """
-        Prueba que traiga todas las comunicaciones de un determinado subtipo.
+        Prueba que traiga todas las comunicaciones_pen de un determinado subtipo.
         """
-        response = self.client.get('/apirest/comunicaciones/?subtipo=PASE A DIPUTADOS')
+        response = self.client.get('/apirest/comunicaciones_pen/?subtipo=NORMA')
         self.assertEqual(response.status_code, self.CODIGO_EXITO)
         self.assertEqual(response.data["results"][0]["tipo"], self.TIPO)
         self.assertEqual(response.data["results"][0]["tipo_camara"], self.TIPO_CAMARA)  
         self.assertEqual(response.data["results"][0]["codigo_origen"], self.CODIGO_ORIGEN)
         self.assertEqual(response.data["results"][0]["codigo_num"], self.CODIGO_NUM)        
-        
-    def test_comunicacion_orden(self):
-        """
-        Prueba que traiga todas las comunicaciones de un determinado orden.
-        """
-        response = self.client.get('/apirest/comunicaciones/?orden=2')
-        self.assertEqual(response.status_code, self.CODIGO_EXITO)
-        self.assertEqual(response.data["results"][0]["tipo"], self.TIPO)
-        self.assertEqual(response.data["results"][0]["tipo_camara"], self.TIPO_CAMARA)  
-        self.assertEqual(response.data["results"][0]["codigo_origen"], self.CODIGO_ORIGEN)
     
     #Test con filtros combinados. 
     def test_codigo_anio_y_codigo_num(self):
         """
-        Prueba que traiga las comunicaciones por un anio y numero determinado.
+        Prueba que traiga las comunicaciones_pen por un anio y numero determinado.
         """
-        response = self.client.get('/apirest/comunicaciones/?codigo_num=0114&codigo_anio=1997')   
+        response = self.client.get('/apirest/comunicaciones_pen/?codigo_num=0001&codigo_anio=2002')   
         self.assertEqual(response.status_code, self.CODIGO_EXITO)
         self.assertEqual(response.data["results"][0]["codigo_num"], self.CODIGO_NUM)
         self.assertEqual(response.data["results"][0]["codigo_anio"], self.CODIGO_ANIO)
         self.assertEqual(response.data["results"][0]["tipo"], self.TIPO)       
         
-    def test_codigo_anio_y_orden(self):
+    def test_codigo_anio_y_subtipo(self):
         """
-        Prueba que traiga las comunicaciones por un anio y orden.
+        Prueba que traiga las comunicaciones_pen por un anio y orden.
         """
-        response = self.client.get('/apirest/comunicaciones/?codigo_anio=2002&orden=2')
+        response = self.client.get('/apirest/comunicaciones_pen/?codigo_anio=2002&orden=2')
         self.assertEqual(response.status_code, self.CODIGO_EXITO)
         self.assertEqual(response.data["results"][0]["tipo"], self.TIPO)
         self.assertEqual(response.data["results"][0]["codigo_anio"], self.CODIGO_ANIO_ORDEN)
-        self.assertEqual(response.data["results"][0]["tipo"], self.TIPO)     
         
