d055c30897b113c9fc61fc0fef3c450f809871c9
FiubaDaniel <fiubadaniel@gmail.com>
2015-06-23 15:59:44 -0300

    Actualizo test de BAE.

--- ServiciosParlamentarios/apirest/tests/publicaciones/boletin_asuntos_entrados.py
+++ ServiciosParlamentarios/apirest/tests/publicaciones/boletin_asuntos_entrados.py
@@ -9,12 +9,14 @@ class TestBoletinAsuntosEntrados(APITestCase):
     CANTIDAD_BAE_POR_NUMERO = 16
     NUMERO = 1
     FK_CAMARA_REUNION = 15
+    FK_CAMARA_REUNION_ID = 12
+    NUMERO_ID = 35
     
     def test_bae_list(self):
         """
         Prueba el servicio para listar los BAE. 
         """
-        response = self.client.get('/apirest/boletin_asuntos_entrados/')
+        response = self.client.get('/apirest/boletines_asuntos_entrados/')
         self.assertEqual(response.status_code, self.CODIGO_EXITO)
         self.assertEqual(response.data["count"], self.CANTIDAD_BAE)
         
@@ -22,7 +24,7 @@ class TestBoletinAsuntosEntrados(APITestCase):
         """
         Prueba el servicio para obtener un BAE filtrando por numero.
         """
-        response = self.client.get("/apirest/boletin_asuntos_entrados/?numero=1")
+        response = self.client.get("/apirest/boletines_asuntos_entrados/?numero=1")
         self.assertEqual(response.status_code, self.CODIGO_EXITO)
         self.assertEqual(response.data["count"], self.CANTIDAD_BAE_POR_NUMERO)
         self.assertEqual(response.data["results"][0]["numero"], self.NUMERO)
@@ -30,10 +32,18 @@ class TestBoletinAsuntosEntrados(APITestCase):
         
     def test_bae_por_id(self):
         """
-        Prueba el servicio para obtener un BAT por id. 
+        Prueba el servicio para obtener un BAE por id. 
         """
-        response = self.client.get("/apirest/boletin_asuntos_entrados/45173/")
+        response = self.client.get("/apirest/boletines_asuntos_entrados/45452/")
         self.assertEqual(response.status_code, self.CODIGO_EXITO)
-        self.assertEqual(response.data["numero"], self.NUMERO)
-        self.assertEqual(response.data["fk_camara_reunion"], self.FK_CAMARA_REUNION)
+        self.assertEqual(response.data["numero"], self.NUMERO_ID)
+        self.assertEqual(response.data["fk_camara_reunion"], self.FK_CAMARA_REUNION_ID)
+       
+    def test_bae_por_tipo(self):
+        """
+        Prueba el servicio para obtener un BAE filtrando por tipo.
+        """
+        response = self.client.get("/apirest/boletines_asuntos_entrados/?tipo=BOLETIN_ASUNTOS_ENTRADOS")
+        self.assertEqual(response.status_code, self.CODIGO_EXITO)
+        self.assertEqual(response.data["count"], self.CANTIDAD_BAE)
         
