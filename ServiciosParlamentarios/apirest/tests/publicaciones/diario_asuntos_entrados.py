58de93f4a14d455a781f5f5dc89f1f7be1568cf3
FiubaDaniel <fiubadaniel@gmail.com>
2015-06-23 16:22:33 -0300

    Modifico los tests de diarios de asuntos entradas.

--- ServiciosParlamentarios/apirest/tests/publicaciones/diario_asuntos_entrados.py
+++ ServiciosParlamentarios/apirest/tests/publicaciones/diario_asuntos_entrados.py
@@ -5,15 +5,15 @@ class TestDiarioAsuntosEntrados(APITestCase):
     
     #Constantes usadas para realizar la comparacion en los test.
     CODIGO_EXITO = 200 
-    CANTIDAD_DAE = 3287
+    CANTIDAD_DAE = 3319
     CANTIDAD_DAE_POR_NUMERO = 28
     NUMERO = 1
 
     def test_bn_list(self):
         """
-        Prueba el servicio para listar los DAE. 
+        Prueba el servicio para listar los BN. 
         """
-        response = self.client.get('/apirest/diario_asuntos_entrados/')
+        response = self.client.get('/apirest/diarios_asuntos_entrados/')
         self.assertEqual(response.status_code, self.CODIGO_EXITO)
         self.assertEqual(response.data["count"], self.CANTIDAD_DAE)
 
@@ -21,15 +21,15 @@ class TestDiarioAsuntosEntrados(APITestCase):
         """
         Prueba el servicio para obtener un BN filtrando por numero.
         """
-        response = self.client.get("/apirest/diario_asuntos_entrados/?numero=1")
+        response = self.client.get("/apirest/diarios_asuntos_entrados/?numero=1")
         self.assertEqual(response.status_code, self.CODIGO_EXITO)
         self.assertEqual(response.data["count"], self.CANTIDAD_DAE_POR_NUMERO)
         self.assertEqual(response.data["results"][0]["numero"], self.NUMERO)
          
     def test_bn_por_id(self):
         """
-        Prueba el servicio para obtener un BAT por id. 
+        Prueba el servicio para obtener un BN por id. 
         """
-        response = self.client.get("/apirest/diario_asuntos_entrados/45908/")
+        response = self.client.get("/apirest/diarios_asuntos_entrados/46188/")
         self.assertEqual(response.status_code, self.CODIGO_EXITO)
         self.assertEqual(response.data["numero"], self.NUMERO)
