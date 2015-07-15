94d62bd2781350fd89c13bf081f7764f16348db7
Giovanni Opromolla <giopromolla@gmail.com>
2015-07-15 15:15:36 -0300

    Elimino logger e implemento el default

--- ServiciosParlamentarios/apirest/exceptions/authorizationErrors.py
+++ ServiciosParlamentarios/apirest/exceptions/authorizationErrors.py
@@ -1,6 +1,6 @@
 from rest_framework import status 
 from apirest.utils.constants import Constants
-    
+import logging    
     
 class NoAuthorizationException(Exception):
     
@@ -8,7 +8,8 @@ class NoAuthorizationException(Exception):
     default_detail = Constants().NO_AUTH_EXCP
     
     def __init__(self):
-#         Logger().w(Constants().NO_AUTH_EXCP)
+        logger = logging.getLogger('apirest')
+        logger.warning(Constants().NO_AUTH_EXCP)
         
         Exception.__init__(self, Constants().NO_AUTH_EXCP)
         
@@ -19,7 +20,8 @@ class NotAuthorizedException(Exception):
     default_detail = Constants().NOT_AUTHORIZED_EXCP
     
     def __init__(self):
-#         Logger().w(Constants().NOT_AUTHORIZED_EXCP)
+        logger = logging.getLogger('apirest')
+        logger.warning(Constants().NOT_AUTHORIZED_EXCP)
         Exception.__init__(self, Constants().NOT_AUTHORIZED_EXCP)
         
         
@@ -29,7 +31,8 @@ class AuthorizationFormatException(Exception):
     default_detail = Constants().AUTH_FORMAT_EXCP
     
     def __init__(self):
-#         Logger().w(Constants().AUTH_FORMAT_EXCP)
+        logger = logging.getLogger('apirest')
+        logger.warning(Constants().AUTH_FORMAT_EXCP)
         Exception.__init__(self, Constants().AUTH_FORMAT_EXCP)
         
 class ServerAuthConnectionException(Exception):
@@ -39,7 +42,8 @@ class ServerAuthConnectionException(Exception):
     default_detail = Constants().AUTH_CONNECTION_EXCP
     
     def __init__(self):
-#         Logger().w(Constants().AUTH_CONNECTION_EXCP)
+        logger = logging.getLogger('apirest')
+        logger.warning(Constants().AUTH_CONNECTION_EXCP)
         Exception.__init__(self, Constants().AUTH_CONNECTION_EXCP)  
         
 
--- ServiciosParlamentarios/apirest/utils/utils.py
+++ ServiciosParlamentarios/apirest/utils/utils.py
@@ -16,17 +16,26 @@ def getServiceName(serv):
     #Regex para parsear la URL del servicio solicitado
     #Ejemplo: 
     #        1) /apirest/cargos/ => serviceName = cargos
-    #        2) /apirest/expedientes/1234/firmantes/ = expedientes-firmantes
+    #        2) /apirest/proyectos/1234/firmantes/ = proyectos-firmantes
+    # Los servicios del estilo 'nombre_servicio', quedan como 'nombreservicio'
     
     serviceName = ''
     
-    matchObj = re.match(r'^/apirest/([a-z]+)/(?:[0-9]+)?/?([a-z]+)?', serv)
+    matchObj = re.match(r'^/apirest/([a-z_]+)/(?:[0-9]+)?/?([a-z_]+)?', serv)
     
     if matchObj:
         serviceName = matchObj.group(1)
         aux_obj = matchObj.group(2)
-        if aux_obj:
-            serviceName += '-' + matchObj.group(2) 
+        
+        #Elimino los guiones bajo de los nombres de los servicios
+        serviceName.replace('_','')
+        
+        # Si es un servicio compuesto (tipo 2)
+        if aux_obj: 
+            #Elimino los guiones bajo de los nombres de los servicios
+            aux_obj.relpace('_','')
+            serviceName += '-' + matchObj.group(2)
+             
                     
     return serviceName 
                 
