b82516eb12a6129c44d080cfb5231e2aad8c976a
Giovanni Opromolla <giopromolla@gmail.com>
2015-07-15 15:18:06 -0300

    limpio manage.py

--- ServiciosParlamentarios/manage.py
+++ ServiciosParlamentarios/manage.py
@@ -1,28 +1,19 @@
 #!/usr/bin/env python
 import os
 import sys
-from apirest.utils.logger import Logger
-from apirest.exceptions.loggingErrors import LoggingFolderException
+import logging
 
 def run_services():
-    
-    try:
-        Logger().init()
-    except LoggingFolderException as e:
-        print e.default_detail
-        return
-        
-    Logger().i('*** Starting ServiciosParlamentarios ***')
-    
+
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "ServiciosParlamentarios.settings")
+    
+    logger = logging.getLogger('apirest')   
     
     from django.core.management import execute_from_command_line
     
     execute_from_command_line(sys.argv)
     
-    ''' Stop the logging thread '''
-    Logger().i('*** Stopping ServiciosParlamentarios ***')
-    Logger().stop()
+    logger.info('*** Stopping ServiciosParlamentarios ***')
 
 if __name__ == "__main__":
     run_services()
