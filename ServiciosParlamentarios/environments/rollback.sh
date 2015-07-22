77e177377eef8d249bd77fd54dc15c8cdd11d8ba
FiubaDaniel <fiubadaniel@gmail.com>
2015-07-22 15:25:35 -0300

    Script de rollback en caso de errores durante el proceso de deploy.

--- ServiciosParlamentarios/environments/rollback.sh
+++ ServiciosParlamentarios/environments/rollback.sh
@@ -5,6 +5,91 @@ red='\E[1;31m'
 green='\E[1;32m'
 wipe="\033[1m\033[0m"
 
-#Eliminar version que no funciona
-#copiar version anterior (por parametro) la carpeta /servicios_parlamentarios/release_VERSION
-#modificar servicio.sh
+usage="
+$(basename "$0") [-h] <ambiente> <version_actual> <version_nueva> <host> <port> -- script para hacer deploy de los Servicios Parlamentarios.
+
+where:
+    -h                  muestra esta ayuda
+    <ambiente>          {local, desa, prod}
+    <version_actual>    versión que esta ejecutandose actualmente en el ambiente
+    <version_nueva>     versión nueva que se ejecutará en el ambiente
+    <host>              ejemplo: api.hcdn.gob.ar
+    <port>              ejemplo: 9000"
+
+while getopts ':hs:' option; do
+    case "$option" in
+      h) echo -e "$red"
+         echo "$usage"
+         echo -e "$wipe"
+         exit 0
+         ;;
+    esac
+done
+shift $((OPTIND - 1))
+    
+if [ ! $# -eq 5 ]; then
+    echo -e "$red"
+    echo "$usage"
+    echo -e "$wipe"
+    exit 0
+fi
+
+echo -e "$green"
+echo "Comenzando a hacer el rollback en $1."
+echo -e "$wipe"
+
+ENVIRONMENT_PATH=/opt/servicios_parlamentarios 
+RELEASES_PATH=releases 
+
+ENVIRONMENT=$1
+RELEASE_ACTUAL=release_$2
+RELEASE_ANTERIOR=release_$3
+HOST=$4
+PORT=$5
+
+#Acceder al directorio de ambientes
+cd $ENVIRONMENT_PATH 
+
+
+if [ ! -d "$RELEASE_ACTUAL" ]; then
+	echo "No existe la version actual"
+	exit 0
+fi
+
+if [ ! -d "$RELEASES_PATH/$RELEASE_ANTERIOR" ]; then 
+	echo "La release a la cual quiere volver no existe"
+	exit 0
+fi
+
+
+#Stop crontab 
+sudo service cron stop
+
+#Detener la aplicacion
+sudo pkill -f $HOST:$PORT 
+
+# Remuevo la version actual.
+sudo rm -rf $RELEASE_ACTUAL 
+
+#Muevo la carpeta de la version anterior a de actual.
+sudo mv $RELEASES_PATH/$RELEASE_ANTERIOR $RELEASE_ANTERIOR
+
+#Editar servicios.sh con VERSION nueva
+sudo sed -i '9s/.*/    nohup python \/opt\/servicios_parlamentarios\/'$RELEASE_ANTERIOR'\/spd-back-end\/ServiciosParlamentarios\/manage.py \\/' servicios.sh
+
+#Restart crontab
+sudo service cron restart
+
+#Remove de rollback.sh script 
+sudo rm rollback.sh
+
+echo -e "$green"
+echo "El script ha terminado de realizar el deploy en $1."
+echo -e "$wipe"
+
+
+
+
+
+
+
