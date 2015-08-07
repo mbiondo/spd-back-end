0e179c76b5fa42581ba184699323dd7dfd90e3f7
Giovanni Opromolla <giopromolla@gmail.com>
2015-08-07 15:56:37 -0300

    Agrego temario a la citacion

--- ServiciosParlamentarios/apirest/models/citacion_temario.py
+++ ServiciosParlamentarios/apirest/models/citacion_temario.py
@@ -1,15 +1,18 @@
 from __future__ import unicode_literals
 from django.db import models
 from apirest.utils.constants import Constants
-# from apirest.models.citacion import Citacion
 
 class CitacionTemario(models.Model):
     id = models.AutoField(primary_key=True,db_column='citacion_temario_id')
-    fk_citacion = models.ForeignKey('Citacion', db_column='fk_citacion')
+    fk_citacion = models.ForeignKey('Citacion', db_column='fk_citacion',related_name='temario')
     orden = models.SmallIntegerField(blank=True, null=True)
     tema = models.TextField(blank=True)
+    art_109 = models.CharField(max_length=1, blank=True, db_column='bart109')
+    sobre_trablas = models.CharField(max_length=1, blank=True, db_column='bsobretrablas')
+    exp_agrupados = models.CharField(max_length=1, blank=True, db_column='bexpagrupados')
 
     class Meta:
         managed = False
         db_table = Constants().CITACION_TEMARIO
-        app_label = Constants().APIREST        
+        app_label = Constants().APIREST
+                
