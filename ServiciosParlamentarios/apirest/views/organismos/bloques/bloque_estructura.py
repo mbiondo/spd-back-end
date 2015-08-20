2e7a4f878797c823e8bd4109d2de9ee5f12985b4
FiubaDaniel <fiubadaniel@gmail.com>
2015-08-20 15:20:23 -0300

    Agrego el filtro a la view de bloque estructura.

--- ServiciosParlamentarios/apirest/views/organismos/bloques/bloque_estructura.py
+++ ServiciosParlamentarios/apirest/views/organismos/bloques/bloque_estructura.py
@@ -1,6 +1,7 @@
 from rest_framework import viewsets
 from apirest.models.organismos.bloques.bloque_estructura import BloqueEstructura
 from apirest.serializers.organismos.bloques.bloque_estructura import BloqueEstructuraSerializer
+from apirest.filters.organismos.bloques.bloque_estructura_filter import BloquEstructuraFilter
 from django.db.models import Q
 from apirest.authorizers.authorizator import has_permission
     
@@ -10,6 +11,7 @@ class BloqueEstructuraViewSet(viewsets.ReadOnlyModelViewSet):
     queryset = BloqueEstructura.objects.all()
     serializer_class = BloqueEstructuraSerializer
     ordering_fields = '__all__'
+    filter_class = BloquEstructuraFilter
     search_fields = ('nombre','nro_integrantes','fecha_inicio','fecha_fin','tipo_camara','nota','sigla')    
                  
     @has_permission                 
