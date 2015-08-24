8350a7808d17191fc8c05234e96782cb71124b41
Giovanni Opromolla <giopromolla@gmail.com>
2015-08-24 16:04:12 -0300

    Numero de periodo en Orden del Dia

--- ServiciosParlamentarios/apirest/serializers/publicaciones/orden_dia.py
+++ ServiciosParlamentarios/apirest/serializers/publicaciones/orden_dia.py
@@ -3,6 +3,11 @@ from apirest.models.publicaciones.orden_dia import OrdenDia
 
 class OrdenDiaSerializer(serializers.ModelSerializer):
     
+    periodo = serializers.SerializerMethodField()
+    
+    def get_periodo(self, obj):
+        return obj.periodo.nro_periodo
+        
     class Meta:
         model = OrdenDia
         fields = ('id','periodo','fecha_impresion','tipo','visibilidad','despacho','numero','anio','fecha_art113')
