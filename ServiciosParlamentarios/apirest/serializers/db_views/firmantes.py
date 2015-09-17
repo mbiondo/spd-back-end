2cabd1cde8597393b616504d02244ebf1dea0977
Giovanni Opromolla <giopromolla@gmail.com>
2015-09-17 15:27:25 -0300

    agrego id a los firmantes

--- ServiciosParlamentarios/apirest/serializers/db_views/firmantes.py
+++ ServiciosParlamentarios/apirest/serializers/db_views/firmantes.py
@@ -11,4 +11,4 @@ class FirmanteSerializer(serializers.ModelSerializer):
     class Meta:
         model = Firmantes
 #         fields = ('id','proyecto','persona_fisica_id','codigo_exp','nombre_leg_func','tipo_camara','cargo','orden','distrito','nombre_del_bloque')
-        fields = ('nombre_leg_func','distrito','nombre_del_bloque')
+        fields = ('id','nombre_leg_func','distrito','nombre_del_bloque')
