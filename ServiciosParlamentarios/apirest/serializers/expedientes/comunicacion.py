315c2ad03ed15a5c45646655f529614df1d44174
FiubaDaniel <fiubadaniel@gmail.com>
2015-10-21 11:19:04 -0300

    Update comunicaciones para que incluya los campos de empedientes.

--- ServiciosParlamentarios/apirest/serializers/expedientes/comunicacion.py
+++ ServiciosParlamentarios/apirest/serializers/expedientes/comunicacion.py
@@ -5,7 +5,8 @@ class ComunicacionSerializer(serializers.ModelSerializer):
     
     class Meta:
         model = Comunicacion 
-        fields= ('id','subtipo','fecha_recepcion','orden')
+        fields= ('id','subtipo','fecha_recepcion','orden','codigo_exp','codigo_num','codigo_origen','codigo_anio','sumario','tipo_camara','tipo','codigo_estado',
+                  'fecha_caducidad','fecha','periodo','titulo','voces')
 
 class ComunicacionChildSerializer(serializers.ModelSerializer):
     
