886913b290932cd66f712b7cfcab7d6dee3503b8
FiubaDaniel <fiubadaniel@gmail.com>
2015-10-21 15:41:52 -0300

    Fix en comunicacion_resultado.

--- ServiciosParlamentarios/apirest/models/expedientes/comunicacion_resultado.py
+++ ServiciosParlamentarios/apirest/models/expedientes/comunicacion_resultado.py
@@ -7,8 +7,11 @@ from apirest.utils.constants import Constants
 class ComunicacionResultado(models.Model):
     id = models.AutoField(primary_key=True,db_column='comunicacion_resultado_id')
     fk_resultado = models.ForeignKey(Resultado, db_column='fk_resultado')
-    fk_comunicacion = models.ForeignKey(Comunicacion, db_column='fk_comunicacion')
+    fk_comunicacion = models.ForeignKey(Comunicacion, db_column='fk_comunicacion')  
+    
     class Meta:
         managed = False
-        db_table = Constants.COMUNICACION_RESULTADO
+        db_table = Constants().COMUNICACION_RESULTADO
         app_label = Constants().APIREST
+        
+    
