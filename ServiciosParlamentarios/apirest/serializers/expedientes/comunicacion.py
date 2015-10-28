9899b16a81783ca4d121e5eb1caa054c375bb6dc
Giovanni Opromolla <giopromolla@gmail.com>
2015-10-28 15:12:31 -0300

    Comunicacion con expediente origen

--- ServiciosParlamentarios/apirest/serializers/expedientes/comunicacion.py
+++ ServiciosParlamentarios/apirest/serializers/expedientes/comunicacion.py
@@ -1,23 +1,21 @@
 from rest_framework import serializers
 from apirest.models.expedientes.comunicacion import Comunicacion
-from apirest.serializers.expedientes.resultado import ResultadoComunicacionSerializer
 
 class ComunicacionSerializer(serializers.ModelSerializer):
-    
-    resultados_com = ResultadoComunicacionSerializer(many=True)
-    
-#     resultados_com = serializers.SerializerMethodField()
-#     
-#     def get_resultados_com(self, obj):
-#         result = None
-#         if obj.resultados_com is not None:
-#             obj.resultados_com = ResultadoComunicacionSerializer( many=True)
-#             result = obj.resultados_com 
-#         
-#         return result
         
+    proyecto_original_id = serializers.SerializerMethodField()
+     
+    def get_proyecto_original_id(self, obj):
+        result = None
+        if obj.resultados_com is not None:
+            for resultado in obj.resultados_com.all():
+                for proyecto in resultado.resultados.all():
+                    result = proyecto.proyecto_id
+   
+        return result
+     
     class Meta:
         model = Comunicacion 
         fields= ('id','codigo_exp','codigo_num','codigo_origen','codigo_anio','sumario','tipo_camara','tipo','codigo_estado',
-                  'fecha_caducidad','fecha','periodo','titulo','voces','subtipo','fecha_recepcion','orden','resultados_com')
+                  'fecha_caducidad','fecha','periodo','titulo','voces','subtipo','fecha_recepcion','orden','proyecto_original_id')
         
