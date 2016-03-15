f247d799bd5736df376b879a9069791b65a5d5f7
santuan <smrcouto88@gmail.com>
2016-03-15 15:09:28 -0300

    CoalesceFilterBackend

--- ServiciosParlamentarios/apirest/filters/custom_filter.py
+++ ServiciosParlamentarios/apirest/filters/custom_filter.py
@@ -2,6 +2,14 @@ from rest_framework.compat import django_filters
 from django.db.models import Q
 
 class CustomFilterList(django_filters.Filter):
+    def filter_queryset(self, request, queryset, view):
+        id_list = request.QUERY_PARAMS.getlist('ids[]')
+        if id_list:
+            # Disable pagination, so all records can load.
+            view.pagination_class = None
+            queryset = queryset.filter(id__in=id_list)
+        return queryset    
+        
     def filter(self, qs, value):
         if value not in (None, ''):
             values = [v for v in value.split(',')]
